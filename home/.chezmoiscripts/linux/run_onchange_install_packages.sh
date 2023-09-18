4ca64a4d76b27657b2440a6235f882e7b704722b
Thomas Fourcroy <thomas.fourcroy@ciose.fr>
2023-09-18 16:33:17 +0200

    maj

--- home/.chezmoiscripts/linux/run_onchange_install_packages.sh
+++ home/.chezmoiscripts/linux/run_onchange_install_packages.sh
@@ -2,16 +2,13 @@
 
 set -eufo pipefail
 set +x
-sudo apt-get install -y ripgrep xz-utils &&
-
-if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://nixos.org/nix/install)"
-elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://nixos.org/nix/install)"
-else
-    echo "To install nixos, you must have curl or wget installed." >&2
-    exit 1
-fi
-#curl -L https://nixos.org/nix/install | sudo sh &&
+sudo apt-get install -y curl ripgrep xz-utils build-essential &&
+sh -c "$(curl -fsSL https://nixos.org/nix/install)" &&
 source $HOME/.nix-profile/etc/profile.d/nix.sh &&
 nix-env -iA nixpkgs.neovim
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3 &&
+rm -rf ~/miniconda3/miniconda.sh &&
+source "$HOME/miniconda3/etc/profile.d/conda.sh" &&
+conda config --set auto_activate_base false
+
