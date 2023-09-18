8e5c8106ea977ae2a228659bc6136a77ff3f8767
Thomas Fourcroy <thomas.fourcroy@ciose.fr>
2023-09-18 16:05:53 +0200

    maj test

--- home/.chezmoiscripts/linux/run_onchange_before_install-packages.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_install-packages.sh
@@ -2,6 +2,14 @@
 
 set -eufo pipefail
 set +x
-curl -L https://nixos.org/nix/install | sudo sh &&
+if [ "$(command -v curl)" ]; then
+    sudo sh -c "$(curl -fsSL https://nixos.org/nix/install)"
+elif [ "$(command -v wget)" ]; then
+    sudo sh -c "$(wget -qO- https://nixos.org/nix/install)"
+else
+    echo "To install nixos, you must have curl or wget installed." >&2
+    exit 1
+fi
+#curl -L https://nixos.org/nix/install | sudo sh &&
 source $HOME/.nix-profile/etc/profile.d/nix.sh &&
 nix-env -iA nixpkgs.neovim
