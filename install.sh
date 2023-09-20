edd8653eb1764363c52026413f5b2d8d954cbb19
kompote <tomtomfatcat@gmail.com>
2023-09-20 11:59:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y curl ripgrep xz-utils build-essential &&
 sh -c "$(curl -fsSL https://nixos.org/nix/install)" &&
 source $HOME/.nix-profile/etc/profile.d/nix.sh &&
 nix-env -iA nixpkgs.neovim &&
-nix-env -i chezmoi &&
+nix-env -iA nixpkgs.chezmoi &&
 
 chezmoi init --apply kompote
