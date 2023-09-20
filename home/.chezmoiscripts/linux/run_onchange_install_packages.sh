78a9d3a2c4a4da1b561504e344d8d68640edd15d
Thomas Fourcroy <thomas.fourcroy@ciose.fr>
2023-09-20 11:46:34 +0200

    maj

--- home/.chezmoiscripts/linux/run_onchange_install_packages.sh
+++ home/.chezmoiscripts/linux/run_onchange_install_packages.sh
@@ -2,14 +2,9 @@
 
 set -eufo pipefail
 set +x
-sudo apt-get install -y curl ripgrep xz-utils build-essential &&
-sh -c "$(curl -fsSL https://nixos.org/nix/install)" &&
-source $HOME/.nix-profile/etc/profile.d/nix.sh &&
-nix-env -iA nixpkgs.neovim &&
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh &&
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3 &&
-rm -rf ~/miniconda3/miniconda.sh &&
+mkdir -p $HOME/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda3/miniconda.sh &&
+bash $HOME/miniconda3/miniconda.sh -b -u -p $HOME/miniconda3 &&
+rm -rf $HOME/miniconda3/miniconda.sh &&
 source "$HOME/miniconda3/etc/profile.d/conda.sh" &&
 conda config --set auto_activate_base false
-
