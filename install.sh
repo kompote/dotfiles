5fdc17b686e0751f3e598d1bc4adcc0fc8e46896
Thomas Fourcroy <thomas.fourcroy@ciose.fr>
2023-09-25 15:07:50 +0200

    maj nvim + treesitter + nix

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ sh -c "$(curl -fsSL https://nixos.org/nix/install)" &&
 source $HOME/.nix-profile/etc/profile.d/nix.sh &&
 nix-env -iA nixpkgs.neovim &&
 nix-env -iA nixpkgs.chezmoi &&
+nix-shell -p gcc --run "nvim -c 'TSUpdate all'" &&
 
 chezmoi init --apply kompote
